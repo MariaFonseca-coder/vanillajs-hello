e2c582475f8c19fdd7e0c3f196958f81f8dd0558
María José Fonseca Castro <mfonsecac052@gmail.com>
2024-09-04 20:36:44 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  /*ONLOAD significa como recargar página acá, que cada que recargue haga:*/
+  /*seleccionamos el documemnto, que el p de tal doc que es html lo modificamos con el innerHTML y le ponemos la función de acá:*/
+  document.querySelector(".excusa").innerHTML = Excusas();
+  /*seleccionamos el botón y dimos un escuchador, cada que escuche un click genere excusa aleatoriamente:*/
+  document.querySelector(".boton").addEventListener("click", () => {
+    document.querySelector(".excusa").innerHTML = Excusas();
+  });
 };
+function Excusas() {
+  /*Hice nueva función para que se generen al azar y así:*/
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ]; /*floor para que sea número entero, random al azar y length para que recorra todo:*/
+  let sujeto = Math.floor(Math.random() * who.length);
+  let verbo = Math.floor(Math.random() * action.length);
+  let que = Math.floor(Math.random() * what.length);
+  let cuando = Math.floor(Math.random() * when.length);
+  /*Retornamos para que se muestre en pantalla:*/
+  return (
+    who[sujeto] + " " + action[verbo] + " " + what[que] + " " + when[cuando]
+  );
+}
